0069429a32b47ee6cc3c37e948f568386fe66eaf
kisnahc.github.io <dev.kisnahc@gmail.com>
2021-11-18 14:12:34 +0900

    중복 제거

--- src/test/java/com/example/TDDexam/MoneyTest.java
+++ src/test/java/com/example/TDDexam/MoneyTest.java
@@ -31,11 +31,14 @@ class MoneyTest {
 
 
     static class Dollar {
-        int amount = 10;
+        int amount;
+
         public Dollar(int amount) {
+            this.amount = amount;
         }
 
         void times(int multiplier) {
+            amount *= multiplier;
         }
     }
 
